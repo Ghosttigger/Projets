fca18d19a9c0acf6397bc703629e7003a844e155
Bouffblood <bgl.antoine@gmail.com>
2013-10-12 15:15:35 +0200

    Update main.py
    Règle tous les bugs présents jusqu'à maintenant.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-#TODO: améliorer la boucle principale, améliorer la class Joueur(), corriger la class Salle()
+#TODO: améliorer la boucle principale, améliorer la class Joueur(), corriger la class Salle(),
+import sys
+
 def sav(label, nom, desc, sortie="0"):
     """Créer un fichier de sauvegarde de salle avec un nom et une description, ex: sav("test", "exemple","Une description") """
     fichier = open(label, "w")
@@ -16,30 +18,28 @@ class Joueur():
         print("attribut non trouvé")
     def __setattr__(self,avant,apres):
         object.__setattr__(self, avant, apres)
-    def save(self):
+    def update(self):
         """Créer un fichier avec le nom du joueur, et son emplacement"""
-        fichier = open(self.nom, "w")
-        fichier.write("[name]"+ self.nom + "\n" )
-        fichier.write("[position]"+ self.position )
+        fichier = open(self.nom, "r")
+        self.nom = fichier.readline().rstrip('\n\r').split("[name]")
+        self.nom = "".join(self.nom)
+        self.position = fichier.readline().rstrip('\n\r').split("[position]")
+        self.position = "".join(self.position)
         fichier.close()
     def deplacer(self, destination):
         """Remplace dans la sauvegarde du joueur, sa position par sa destination ex: Joueur.deplacer("Olaf","valhalla")"""
-        fichier=open(self.nom, "r")
-        position = fichier.readline().rstrip('\n\r').split("[position]")
-        x=fichier.read()
-        fichier.close()
-        position="".join(position)
-        x=x.replace(position, destination)
+        self.position=self.position.replace(self.position, destination)
         fichier=open(self.nom, "w")
-        fichier.write(x)
+        fichier.write("[name]"+ self.nom + "\n" ) #à cause de l'option "w", obligé de réécrire le fichier
+        fichier.write("[position]"+self.position)
         fichier.close()
     def boussole(self):
         """Renvoie la position courante du Joueur grâce à son fichier de sauvegarde"""
         fichier=open(self.nom, "r")
-        x = fichier.read().rstrip('\n').split("[name]"+self.nom)
+        x = fichier.read().rstrip('\n\r').split("[name]"+self.nom + "\n")
         fichier.close()
         x = "".join(x)
-        self.position = x.replace('\n[position]', '')
+        self.position = x.replace('[position]', '')
         return self.position
 
 class Salle():
@@ -53,36 +53,16 @@ class Salle():
         print("attribut non trouvé")
     def __setattr__(self,avant,apres):
         object.__setattr__(self, avant, apres)
-    def creer(self, label, nom, desc, sortie="0"):
+    def update(self):
         """Créer un fichier de sauvegarde de salle avec un nom et une description, ex: sav("test", "exemple","Une description") """
-        fichier = open(label, "w")
-        fichier.write("[name]"+nom+ "\n" )
-        fichier.write("[description]"+desc+ "\n" )
-        fichier.write("[sortie]"+sortie)
-        fichier.close()
-    def nom(self):
-        """renvoie le nom de la salle grâce à son fichier de sauvegarde"""
-        fichier=open(self.label,"r")
+        fichier = open(self.label, "r")
         self.nom = fichier.readline().rstrip('\n\r').split("[name]")
         self.nom = "".join(self.nom)
-        fichier.close()
-        return self.nom
-    def description(self):
-        """renvoie la description d'une salle grâce à son fichier de sauvegarde"""
-        fichier=open(self.label,"r")
-        self.description = fichier.readline().rstrip('\n\r').split("[description]")
-        self.description = "".join(description)
-        fichier.close()
-        return self.description
-    def sortie(self):
-        """renvoie la sortie d'une salle grâce à son fichier de sauvegarde"""
-        fichier=open(self.label,"r")
+        self.position = fichier.readline().rstrip('\n\r').split("[description]")
+        self.description = "".join(self.description)
         self.sortie = fichier.readline().rstrip('\n\r').split("[sortie]")
         self.sortie = "".join(self.sortie)
         fichier.close()
-        print(self.sortie)
-        return self.sortie
-
 def lire(file):
     """Lit un fichier créé avec la fonction sav() et affiche son nom et sa description"""
     fichier=open(file,"r")
@@ -93,39 +73,28 @@ def lire(file):
     nom = "".join(nom)
     description="".join(description)
     sortie="".join(sortie)
-    print("===="+nom+"====")
+    print("===="+nom+"====\n")
     print(description+"\n")
     print("sorties:")
     print(sortie)
 
-
-Test=Salle()
-Test.creer("depart","exemple","Blablabla","deux")
-
-#Création de la salle DEUX
-Deux=Salle()
-Deux.creer("deux","exempledeux","abracadabra !","test")
-
-#Création du joueur Bouffblood
+#Création de la salle courante
+Room=Salle()
+#Création du joueur 1
 j1=Joueur()
-j1.nom="Olaf"
-j1.save()
+j1.update()
 
-
+j1.deplacer("deux")
 
 continuer=1
 while continuer:
-    pos=j1.boussole()
-    lire(pos)
-    Test.sortie="deux"
-    j1.deplacer("deux")
-    go=input("> ")
-    if go == Test.sortie:
-        j1.deplacer(go)
-    else:
-        print("La destination n'existe pas.")
-    
-
-
-
-
+            Room.label=j1.boussole()
+            Room.update()
+            lire(j1.boussole())
+            go = input("> ")
+            if go == "exit":
+                sys.exit()
+            elif go == Room.sortie:
+                j1.deplacer(Room.sortie)
+            else:
+                print("Cette destination n'existe pas")
