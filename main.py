9894d43acae5df46fddcaada68d3ab9b6651b131
Bouffblood <bgl.antoine@gmail.com>
2013-10-12 18:36:06 +0200

    Update main.py
    ajout de la gestion de l'inventaire

--- main.py
+++ main.py
@@ -3,28 +3,34 @@
 import sys
 
 class Joueur():
-    """Permet de créer, deplacer le joueur"""
-    def __init__(self, nom="Olaf", position="depart"):
+    """Gère tout ce qui est relatif au joueur"""
+    def __init__(self, nom="Olaf", position="depart", inventaire=""):
         self.nom=nom
         self.position=position
+        self.inventaire=inventaire
     def __getattr__(self):
         print("attribut non trouvé")
     def __setattr__(self,avant,apres):
         object.__setattr__(self, avant, apres)
     def update(self):
-        """mise à jour des attributs du joueur, depuis son fichier de sauvegarde"""
+        """Créer un fichier avec le nom du joueur, et son emplacement"""
         fichier = open(self.nom, "r")
         self.nom = fichier.readline().rstrip('\n\r').split("[name]")
         self.nom = "".join(self.nom)
         self.position = fichier.readline().rstrip('\n\r').split("[position]")
         self.position = "".join(self.position)
+        self.inventaire = fichier.readline().rstrip('\n\r').split("[inventaire]")
+        self.inventaire = "".join(self.inventaire)
+        self.inventaire = self.inventaire.split(',')
+        print(self.inventaire)
         fichier.close()
     def deplacer(self, destination):
         """Remplace dans la sauvegarde du joueur, sa position par sa destination ex: Joueur.deplacer("Olaf","valhalla")"""
         self.position=self.position.replace(self.position, destination)
         fichier=open(self.nom, "w")
-        fichier.write("[name]"+ self.nom + "\n" ) #à cause de l'option "w", obligé de réécrire le fichier
-        fichier.write("[position]"+self.position)
+        fichier.write("[name]"+ self.nom + "\n") #à cause de l'option "w", obligé de réécrire le fichier
+        fichier.write("[position]"+self.position + "\n")
+        fichier.write("[inventaire]"+ ",".join(self.inventaire))
         fichier.close()
     def boussole(self):
         """Renvoie la position courante du Joueur grâce à son fichier de sauvegarde"""
@@ -32,7 +38,6 @@ class Joueur():
         x = fichier.read().rstrip('\n\r').split("[name]"+self.nom + "\n")
         fichier.close()
         x = "".join(x)
-        self.position = x.replace('[position]', '')
         return self.position
 
 class Salle():
@@ -47,7 +52,7 @@ class Salle():
     def __setattr__(self,avant,apres):
         object.__setattr__(self, avant, apres)
     def update(self):
-        """mise à jour des attributs de la salle courante"""
+        """Créer un fichier de sauvegarde de salle avec un nom et une description, ex: sav("test", "exemple","Une description") """
         fichier = open(self.label, "r")
         self.nom = fichier.readline().rstrip('\n\r').split("[name]")
         self.nom = "".join(self.nom)
@@ -55,10 +60,10 @@ class Salle():
         self.description = "".join(self.description)
         self.sortie = fichier.readline().rstrip('\n\r').split("[sortie]")
         self.sortie = "".join(self.sortie)
-        self.sortie = self.sortie.split(' ')
+        self.sortie = self.sortie.split(',')
         fichier.close()
 def lire(file):
-    """Lit un fichier de salle et affiche son nom et sa description"""
+    """Lit un fichier créé avec la fonction sav() et affiche son nom et sa description"""
     fichier=open(file,"r")
     nom = fichier.readline().rstrip('\n\r').split("[name]")
     description = fichier.readline().rstrip('\n\r').split("[description]")
@@ -87,11 +92,16 @@ while continuer:
     x = Room.sortie.count(go)
     if go == "exit":
         sys.exit()
+    elif go == "inventaire":
+        print("Dans votre sac : " + ",".join(j1.inventaire))
     compteur = -1
     for i in Room.sortie:
         compteur += 1
         i = "".join(i)
-        if go == i:
+        if go == i :
             j1.deplacer(go)
-        elif x == 0:
+        elif x == 0 and go != "inventaire":
             print("Cette destination n'existe pas")
+
+
+
