341ef898b858d79fda5a0d93a2c89f3604ba28b8
Bouffblood <bgl.antoine@gmail.com>
2013-10-13 15:36:45 +0200

    Update main.py
    Ajout: Quêtes ! \o/
    Les fonctions sont dans un fichier séparé.

--- main.py
+++ main.py
@@ -1,98 +1,7 @@
 #!/usr/bin/env python3
-
+# -*- coding:utf-8 -*-
+from fonctions import *
 import sys
-
-class Joueur():
-    """Gère tout ce qui est relatif au joueur"""
-    def __init__(self, nom="Olaf", position="depart", inventaire=""):
-        self.nom=nom
-        self.position=position
-        self.inventaire=inventaire
-    def __getattr__(self):
-        print("attribut non trouvé")
-    def __setattr__(self,avant,apres):
-        object.__setattr__(self, avant, apres)
-    def update(self):
-        """Créer un fichier avec le nom du joueur, et son emplacement"""
-        fichier = open(self.nom, "r")
-        self.nom = fichier.readline().rstrip('\n\r').split("[name]")
-        self.nom = "".join(self.nom)
-        self.position = fichier.readline().rstrip('\n\r').split("[position]")
-        self.position = "".join(self.position)
-        self.inventaire = fichier.readline().rstrip('\n\r').split("[inventaire]")
-        self.inventaire = "".join(self.inventaire)
-        self.inventaire = self.inventaire.split(',')
-        fichier.close()
-    def deplacer(self, destination):
-        """Remplace dans la sauvegarde du joueur, sa position par sa destination ex: Joueur.deplacer("Olaf","valhalla")"""
-        self.position=self.position.replace(self.position, destination)
-        fichier=open(self.nom, "w")
-        fichier.write("[name]"+ self.nom + "\n") #à cause de l'option "w", obligé de réécrire le fichier
-        fichier.write("[position]"+self.position + "\n")
-        fichier.write("[inventaire]"+ ",".join(self.inventaire))
-        fichier.close()
-    def boussole(self):
-        """Renvoie la position courante du Joueur grâce à son fichier de sauvegarde"""
-        fichier=open(self.nom, "r")
-        x = fichier.read().rstrip('\n\r').split("[name]"+self.nom + "\n")
-        fichier.close()
-        x = "".join(x)
-        return self.position
-    def ajouter(self, objet):
-        """Ajoute un objet à l'inventaire, et l'enregistre dans le fichier du joueur"""
-        self.inventaire.append(objet)
-        self.inventaire.sort()
-        fichier=open(self.nom, "w")
-        fichier.write("[name]"+ self.nom + "\n") #à cause de l'option "w", obligé de réécrire le fichier
-        fichier.write("[position]"+self.position + "\n")
-        fichier.write("[inventaire]"+ ",".join(self.inventaire))
-        fichier.close()
-    def enlever(self, objet):
-        """Enlève un objet à l'inventaire, et enregistre les modifications dans le fichier du joueur"""
-        self.inventaire.remove(objet)
-        self.inventaire.sort()
-        fichier=open(self.nom, "w")
-        fichier.write("[name]"+ self.nom + "\n") #à cause de l'option "w", obligé de réécrire le fichier
-        fichier.write("[position]"+self.position + "\n")
-        fichier.write("[inventaire]"+ ",".join(self.inventaire))
-        fichier.close()        
-
-class Salle():
-    """Gère le nom, la description et la sortie des salles, grâce à leurs fichiers de sauvegarde"""
-    def __init__(self,label="RAF", nom="", description="", sortie="0"):
-        self.nom=nom
-        self.description=description
-        self.sortie=sortie
-        self.label=label
-    def __getattr__(self):
-        print("attribut non trouvé")
-    def __setattr__(self,avant,apres):
-        object.__setattr__(self, avant, apres)
-    def update(self):
-        """Créer un fichier de sauvegarde de salle avec un nom et une description, ex: sav("test", "exemple","Une description") """
-        fichier = open(self.label, "r")
-        self.nom = fichier.readline().rstrip('\n\r').split("[name]")
-        self.nom = "".join(self.nom)
-        self.position = fichier.readline().rstrip('\n\r').split("[description]")
-        self.description = "".join(self.description)
-        self.sortie = fichier.readline().rstrip('\n\r').split("[sortie]")
-        self.sortie = "".join(self.sortie)
-        self.sortie = self.sortie.split(',')
-        fichier.close()
-def lire(file):
-    """Lit un fichier créé avec la fonction sav() et affiche son nom et sa description"""
-    fichier=open(file,"r")
-    nom = fichier.readline().rstrip('\n\r').split("[name]")
-    description = fichier.readline().rstrip('\n\r').split("[description]")
-    sortie = fichier.readline().rstrip('\n\r').split("[sortie]")
-    fichier.close()
-    nom = "".join(nom)
-    description="".join(description)
-    sortie="".join(sortie)
-    print("===="+nom+"====\n")
-    print(description+"\n")
-    print("sorties:")
-    print(sortie)
 
 #Création de la salle courante
 Room=Salle()
@@ -106,8 +15,18 @@ while continuer:
     Room.label = j1.boussole()
     Room.update()
     lire(j1.boussole())
+    fichier=open("quetes", "r")
+    for ligne in fichier:
+        for quete in j1.quetes:
+            if quete in ligne:
+                quete_verif = ligne
+                quete_verif = quete_verif.rstrip('\n').split("::")
+                quete_verif = "".join(quete_verif)
+                quete_verif = quete_verif.split(quete)
+                quete_verif = "".join(quete_verif)
+                exec(open(quete_verif).read())
+    fichier.close()
     go = input("> ")
-    x = Room.sortie.count(go)
     if go == "exit":
         sys.exit()
     elif go == "inventaire":
@@ -126,12 +45,13 @@ while continuer:
                     i = len(j1.inventaire)
                 
             i += 1
+    elif go == "quetes":
+        for i in j1.quetes:
+            i = "".join(i)
+            print("Quête en cours : " + i)
     for i in Room.sortie:
         i = "".join(i)
-        if go == i :
+        if go == i:
             j1.deplacer(go)
-        elif x == 0 and go != "inventaire":
+        elif go != "inventaire" and go != "quetes":
             print("Cette destination n'existe pas")
-
-
-
