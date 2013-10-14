efef8cb3bc5563344adcd8f1fb56989ab9ad0d5f
Bouffblood <bgl.antoine@gmail.com>
2013-10-14 12:14:32 +0200

    Update main.py
    Ajout des graphismes (non terminé)

--- main.py
+++ main.py
@@ -2,18 +2,42 @@
 # -*- coding:utf-8 -*-
 from fonctions import *
 import sys
-
+import pygame
+from pygame.locals import *
+pygame.init()
 #Création de la salle courante
 Room=Salle()
 #Création du joueur 1
 j1=Joueur()
 j1.update()
 
+#Pour afficher du texte à l'écran :
+jaune = (255, 255, 0)
+police = pygame.font.SysFont("Comic Sans MS", 15)
+texte = police.render("Ce programme est fantastique !", 1, jaune)
+
+#Pour l'écran
+bordure_paysage = pygame.image.load("bordure_paysage.bmp")
+bordure_perso = pygame.image.load("bordure_personnage2.bmp")
+bordure_paysage.set_colorkey((255,0,255))
+bordure_perso.set_colorkey((255,0,255))
+fenetre = pygame.display.set_mode((640,480))
+pygame.display.set_caption("Moteur de livre dont vous êtes le héros")
+
+
 continuer=1
 while continuer:
     j1.update()
     Room.label = j1.boussole()
     Room.update()
+    paysage=pygame.image.load(Room.image)
+    paysage = pygame.transform.scale(paysage, (412,300))
+    fenetre.blit(paysage,(12,17))
+    fenetre.blit(bordure_paysage,(5,5))
+    fenetre.blit(bordure_perso,(450,0))
+    pygame.display.update()
+
+
     lire(j1.boussole())
     fichier=open("quetes", "r")
     for ligne in fichier:
@@ -53,5 +77,6 @@ while continuer:
         i = "".join(i)
         if go == i:
             j1.deplacer(go)
-        elif go != "inventaire" and go != "quetes":
+        elif go != "inventaire" and go != "quetes" and go not in Room.sortie:
             print("Cette destination n'existe pas")
+        
